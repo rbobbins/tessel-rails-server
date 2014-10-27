f0b8b56de40f514de13a68786e872ed333f34389
Rachel Bobbins <rachelheidi@gmail.com>
2014-10-26 18:20:48 -0700

    Responses now include content_type: application/json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  respond_to :json
+  
   protect_from_forgery with: :null_session
 end
